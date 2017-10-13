a4d6e54b2b27fc349f605fa5870bbc8dfb2f31c5
zhufengzhufeng <894918097@qq.com>
2017-10-13 17:42:25 +0800

    'webpack'

--- webpack-pro/src/main.js
+++ webpack-pro/src/main.js
@@ -11,3 +11,47 @@ fn();
 import {str} from './a.js';
 import w from './a.js';
 console.log(w,str);
+
+let arr = [1,2,3];
+let arr1 = [4,5,6];
+console.log([...arr,...arr1]); //es6语法
+
+let obj = {name:1};
+let obj2 = {age:2};
+console.log({...obj,...obj2}); // es7语法
+
+
+// es6 stage-4 stage-3 stage-2 stage-1 stage-0
+
+
+//把css文件当作模块处理引入到main中
+import './index.css';
+
+// 把less文件当作模块插入到页面中
+
+import './style.less';
+
+// 不能在js中直接使用图片地址，否则会导致404 因为webpack认为是字符串，不会进行解析,在js中使用图片需要import
+
+import jpg from './1.jpg';
+let oImg = document.createElement('img');
+oImg.src = jpg;
+document.body.appendChild(oImg);
+
+let oImg1 = document.createElement('img');
+oImg1.src ='https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png' ;
+document.body.appendChild(oImg1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- webpack-pro/webpack.config.js
+++ webpack-pro/webpack.config.js
@@ -1,5 +1,6 @@
 // 要默认导出这样一个配置对象，当执行npm run start时会执行此文件
 let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
    entry:'./src/main.js',
    output:{
@@ -9,7 +10,23 @@ module.exports = {
    // 解析es6语法 需要提供对应模块的规则
    module:{
        rules:[ //设置对应的规则,js用babel-loader进行翻译,node_modules文件夹不需要翻译
-           {test:/\.js$/,use:'babel-loader',exclude:/node_modules/}
+           {test:/\.js$/,use:'babel-loader',exclude:/node_modules/},
+           // style-loader 将转化好的css插入到style标签内
+           // css-loader 用来解析css模块的
+           // 从右往左写 顺序不能写反
+           {test:/\.css$/,use:['style-loader','css-loader']},
+           {test:/\.less$/,use:['style-loader','css-loader','less-loader']},
+           // 一般情况下8k以下的我们会转化成base64
+           // limit可以限制图片超过此值时不进行转化
+           // limit超过limit的值会把图片生产出来，如果小于会转成base64
+           {test:/\.(jpg|png|gif)$/,use:'url-loader?limit=5192'}
        ]
-   }
+   },
+   plugins:[ // 配置插件
+       // 会找到模板文件将打包后的结果插入到html中，最后产出到output的path中
+        new HtmlWebpackPlugin({
+            template:'./index.html',
+            filename:'index.html'
+        })
+   ]
 };
