0d1d69ea0edb7d4c4a44304ecdafe9e2f5774a68
zhufengzhufeng <894918097@qq.com>
2017-10-13 10:08:49 +0800

    'esm'

---
+++ esm/a.js
@@ -0,0 +1,5 @@
+//一个个导出 会将这个str放到一个对象中 {str:'我很帅',b:func}
+export let str = '我很帅';
+export function b() {
+    console.log(1);
+}
---
+++ esm/main.js
@@ -0,0 +1,10 @@
+// import有声明的功能
+import {b,str} from './a.js'
+console.log(str);
+b();
+
+
+
+
+/*
+let {str,a:b} = {str:'我很帅',a:1};*/
