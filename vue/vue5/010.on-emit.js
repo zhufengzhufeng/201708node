9056f68a2a3b678989ada13150e294bd2b0dacb7
zhufengzhufeng <894918097@qq.com>
2017-10-17 16:17:15 +0800

    event

--- vue/vue5/010.on-emit.js
+++ vue/vue5/010.on-emit.js
@@ -9,17 +9,22 @@ Girl.prototype.on = function (eventName,callback) {
     if(this._events[eventName]){ //看看对象中是否存在这个事件
         this._events[eventName].push(callback); //  {女生失恋:[shopping,cry]}
     }else{
-        this._events[eventName] = [callback] // {女生失恋:[shopping]}
+        this._events[eventName] = [callback]; // {女生失恋:[shopping]}
     }
 };
-Girl.prototype.emit = function (eventName,...args) {
+Girl.prototype.emit = function (eventName,...args) { // 剩余运算符
     if(this._events[eventName]){ //如果绑定过这样一个数组
-        this._events[eventName].forEach(cb=>cb());
+        this._events[eventName].forEach(cb=>cb(...args)); // 拓展运算符
+    }
+};
+Girl.prototype.off = function (eventName,callback) { //取消订阅
+    if(this._events[eventName]){
+        this._events[eventName] = this._events[eventName].filter(cb=>cb!==callback);
     }
 };
 let girl = new Girl();
-let shopping = function (val) {console.log('购物');};
-let cry = function (val) {console.log('cry')};
+let shopping = function (val) {console.log('购物',val);};
+let cry = function (val) {console.log('cry',val)};
 girl.on('女生失恋',shopping);
-girl.on('女生失恋',cry);
+girl.off('女生失恋',shopping);
 girl.emit('女生失恋','我');
